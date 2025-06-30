68b564c20815d12a410854db7b0aa96ffaef1d98
msunkaraDTT <mohith.sunkara@digitaltwin.technology>
2025-06-30 23:36:41 +0200

    made changes to next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,13 +1,13 @@
+// FILE: client/next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 's3.amazonaws.com',
-        pathname: '/celestia-ms-101/**'
+        // This wildcard pattern correctly allows any bucket from s3.amazonaws.com
+        hostname: '*.s3.amazonaws.com',
       },
     ],
   },
